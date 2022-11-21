f4e0f54c1a060c6806aac53bb8ccf945187c75fd
doughnut <malika240502@gmail.com>
2022-11-21 20:34:39 +0600

    Fix record bug

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 function removeFromDb(item){
-    fetch(`/delete?item=${item}`, {method: "Delete"}).then(res =>{
+    fetch(`/record?item=${item}`, {method: "Delete"}).then(res =>{
         if (res.status == 200){
             window.location.pathname = "/record"
         }
@@ -9,7 +9,7 @@ function removeFromDb(item){
  function updateDb(item) {
     let input = document.getElementById(item)
     let newitem = input.value
-    fetch(`/update?olditem=${item}&newitem=${newitem}`, {method: "PUT"}).then(res =>{
+    fetch(`/record?olditem=${item}&newitem=${newitem}`, {method: "PUT"}).then(res =>{
         if (res.status == 200){
         alert("Database updated")
             window.location.pathname = "/record"
